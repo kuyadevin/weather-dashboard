9f3d470e2ebc71ae40874d5eb23639c6a840d3d2
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-10 21:13:53 -0700

    created button with city name

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,15 @@ var city= JSON.stringify(document.getElementById("input").value)
 
 
 function showInput(event){
-    var city= (document.getElementById("input").value)
+    var city= document.getElementById("input").value
     event.preventDefault();
     localStorage.setItem("location", city);
-    var cityButton= $("<button>").addClass(oldCity,"form-control", "cityButton");
-    var oldCity = cityButton.textContent=city;
-    $(".col-3").append(cityButton)
+
+    var cityButton= $("<button>").addClass("form-control", "cityButton");
+    $(".col-3").append(cityButton);
+    $(cityButton).text(city);
+    console.log(city)
+    
     
 }
 button.addEventListener("click", showInput)
