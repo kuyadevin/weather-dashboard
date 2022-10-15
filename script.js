d49378e93fe15c50f3f28d760df71277b09de566
KuyaDevin <devinnguyen@ucsb.edu>
2022-10-15 15:19:45 -0700

    Fix resposivness

--- script.js
+++ script.js
@@ -73,9 +73,9 @@ function showWeatherData(data) {
   data.daily.forEach((day, index) => {
     if (index == 0) {
       currentTempEl.innerHTML = `
+      <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
       <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather w-icon" class="w-icon">
       <div class="other">
-        <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
         <div class="temp">Night - ${day.temp.night}&#176; F</div>
         <div class="temp">Day - ${day.temp.day}&#176; F</div>
       </div>
